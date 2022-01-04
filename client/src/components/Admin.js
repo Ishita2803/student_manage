4a790e2db6bc54c05ff4f59e719fd3300deb97c7
anushikabala <anushikabala@gmail.com>
2022-01-04 23:27:25 +0530

    ,,\"

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
+import './form.css'
+
 
 export class Admin extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <><div class="background">
+                <div class="shape"></div>
+                <div class="shape"></div>
+            </div><form style={{ height: "350px", width: "500px" }}>
+                    <h3 className="align">Hello Admin</h3>
+
+
+                    <label>Email ID: </label>
+                    <input type="email"
+                         />
+
+                    <label>Password: </label>
+                    <input type="password"
+                        />
+                    <br />
+                    <button type="submit">Login</button>
+                    
+
+                </form>
+                </>
+            
         )
     }
 }
