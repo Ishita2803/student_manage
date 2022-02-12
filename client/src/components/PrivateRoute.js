4cbdc54a017fd5c01a4618058e3421309e2d7c86
aakankshabhatt <aakankshabhatt21@gmail.com>
2022-02-12 18:06:34 +0530

    Private route added

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -8,8 +8,6 @@ const PrivateRoute = ({component: Component, ...rest}) => {
     
     return (
 
-        // Show the component only when the user is logged in
-        // Otherwise, redirect the user to /signin page
         <Route {...rest} render={props => (
             isLogin === 'true' ?
             <Component {...props} />
