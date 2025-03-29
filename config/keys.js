0d0f2498c0e2cf6e921c485da186f1572df5ca2c
Ishita Bhargava <68944355+Ishita2803@users.noreply.github.com>
2025-03-29 12:12:15 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://ishita2803:qwerty01@deployment0.jqgf0v4.mongodb.net/?retryWrites=true&w=majority&appName=deployment0" ,
+    mongoURI: "mongodb+srv://ishita2803:qwerty01@deployment0.jqgf0v4.mongodb.net/myFirstDB?retryWrites=true&w=majority&appName=deployment0" ,
     secretOrKey: "secret"
   };
